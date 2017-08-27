39d413a0b8e9c1f071cd53a9e51c136f166ae111
TianJingDa <5304654@sina.com>
2017-08-27 23:08:56 +0800

    设置界面UI基本搭建完毕

--- Assets/Script/Gui/GuiWrapper/SetUpFrameWrapper.cs
+++ Assets/Script/Gui/GuiWrapper/SetUpFrameWrapper.cs
@@ -7,24 +7,67 @@ using UnityEngine.UI;
 /// </summary>
 public class SetUpFrameWrapper : GuiFrameWrapper
 {
+    private GameObject strategyWin;
+    private GameObject languageWin;
+    private GameObject skinWin;
+    private GameObject layoutWin;
+    private GameObject feedbackWin;
+    private GameObject aboutUsWin;
+    private GameObject thankDevelopersWin;
     void Start () 
 	{
         base.id = GuiFrameID.SetUpFrame;
         InitGui();
+        strategyWin = CommonTool.GetGameObjectByName(gameObject, "StrategyWin");
+        languageWin = CommonTool.GetGameObjectByName(gameObject, "LanguageWin");
+        skinWin = CommonTool.GetGameObjectByName(gameObject, "SkinWin");
+        layoutWin = CommonTool.GetGameObjectByName(gameObject, "LayoutWin");
+        feedbackWin = CommonTool.GetGameObjectByName(gameObject, "FeedbackWin");
+        aboutUsWin = CommonTool.GetGameObjectByName(gameObject, "AboutUsWin");
+        thankDevelopersWin = CommonTool.GetGameObjectByName(gameObject, "ThankDevelopersWin");
     }
 
-    void Update () 
-	{
-		
-	}
 
     public override void OnClick(Button btn)
     {
         base.OnClick(btn);
         switch (btn.name)
         {
-            case "CloseBtn":
+            case "AboutUsBtn":
+            case "AboutUs2SetUpFrameBtn":
+                aboutUsWin.SetActive(!aboutUsWin.activeSelf);
+                break;
+            case "FeedbackBtn":
+            case "FeedbackWin":
+                feedbackWin.SetActive(!feedbackWin.activeSelf);
+                break;
+            case "LanguageBtn":
+            case "Language2SetUpFrameBtn":
+                languageWin.SetActive(!languageWin.activeSelf);
+                break;
+            case "LayoutBtn":
+            case "Layout2SetUpFrameBtn":
+                layoutWin.SetActive(!layoutWin.activeSelf);
+                break;
+            case "AboutUs2StartFrameBtn":
+            case "Language2StartFrameBtn":
+            case "Layout2StartFrameBtn":
+            case "SetUp2StartFrameBtn":
+            case "Skin2StartFrameBtn":
+            case "Strategy2StartFrameBtn":
                 GameManager.Instance.SwitchWrapper(GuiFrameID.SetUpFrame, GuiFrameID.StartFrame);
+                break;
+            case "SkinBtn":
+            case "Skin2SetUpFrameBtn":
+                skinWin.SetActive(!skinWin.activeSelf);
+                break;
+            case "StrategyBtn":
+            case "Strategy2SetUpFrameBtn":
+                strategyWin.SetActive(!strategyWin.activeSelf);
+                break;
+            case "ThankDevelopersBtn":
+            case "ThankDevelopersWin":
+                thankDevelopersWin.SetActive(!thankDevelopersWin.activeSelf);
                 break;
             default:
                 Debug.LogError("Can not find Button:" + btn.name);
