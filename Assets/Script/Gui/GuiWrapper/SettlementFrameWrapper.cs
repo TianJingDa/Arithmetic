9674efb01fb101f24610729faaa9a52f0a45d71a
TianJingDa <5304654@sina.com>
2017-09-16 22:49:56 +0800

    开始完善结算界面逻辑

--- Assets/Script/Gui/GuiWrapper/SettlementFrameWrapper.cs
+++ Assets/Script/Gui/GuiWrapper/SettlementFrameWrapper.cs
@@ -7,16 +7,25 @@ using UnityEngine.UI;
 /// </summary>
 public class SettlementFrameWrapper : GuiFrameWrapper
 {
+    private Text settlementTime_Text;
+    private Text settlementAmount_Text;
+    private Text settlementAccuracy_Text;
+    private InfiniteList settlementGrid;
 
     void Start () 
 	{
         id = GuiFrameID.SettlementFrame;
         Init();
+        settlementTime_Text.text = settlementTime_Text.text.Replace("{0}", GameManager.Instance.CurTimeCost.ToString());
+        settlementAmount_Text.text = settlementAmount_Text.text.Replace("{0}", GameManager.Instance.CurResultList.Count.ToString());
     }
 
     protected override void OnStart(Dictionary<string, GameObject> GameObjectDict)
     {
-
+        settlementGrid          = GameObjectDict["SettlementGrid"].GetComponent<InfiniteList>();
+        settlementTime_Text     = GameObjectDict["SettlementTime_Text"].GetComponent<Text>();
+        settlementAmount_Text   = GameObjectDict["SettlementAmount_Text"].GetComponent<Text>();
+        settlementAccuracy_Text = GameObjectDict["SettlementAccuracy_Text"].GetComponent<Text>();
     }
 
 
