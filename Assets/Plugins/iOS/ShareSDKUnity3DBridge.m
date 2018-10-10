e7aef16aa0a8ef0de35aa9f508867d3bf26980d3
TianJingda <5304654@sina.com>
2018-10-10 21:21:06 +0800

    取消无用的定义

--- Assets/Plugins/iOS/ShareSDKUnity3DBridge.m
+++ Assets/Plugins/iOS/ShareSDKUnity3DBridge.m
@@ -22,10 +22,10 @@
 #define __SHARESDK_WECHAT__
 #define __SHARESDK_QQ__
 #define __SHARESDK_SINA_WEIBO__
-#define __SHARESDK_RENREN__
+//#define __SHARESDK_RENREN__
 //#define __SHARESDK_KAKAO__
-#define __SHARESDK_YIXIN__
-#define __SHARESDK_FACEBOOK_MSG__
+//#define __SHARESDK_YIXIN__
+//#define __SHARESDK_FACEBOOK_MSG__
 //#define __SHARESDK_ALISOCIAL__
 //#define __SHARESDK_DINGTALK__
 //#define __SHARESDK_MEIPAI__
